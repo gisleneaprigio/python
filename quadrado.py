c1fab6b16227d0fc916183c726338c01ffd23121
gisleneaprigio <gisleneaprigio@gmail.com>
2021-03-24 22:19:02 -0300

    primeiros exercicios

---
+++ quadrado.py
@@ -0,0 +1,5 @@
+ladoQuadrado = int(input("Digite o valor correspondente ao lado de um quadrado: "))
+perimetroQuadrado = ladoQuadrado*4
+areaQuadrado = ladoQuadrado**2
+
+print("perímetro:", perimetroQuadrado,"- área:", areaQuadrado)

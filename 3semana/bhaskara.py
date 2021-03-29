47add28ada0100e1b7991ddf7df247dee0e88498
gisleneaprigio <gisleneaprigio@gmail.com>
2021-03-29 14:36:18 -0300

    terminando 3semana

--- 3semana/bhaskara.py
+++ 3semana/bhaskara.py
@@ -1,17 +1,20 @@
-a = float(input('Entre com o valor de a: '))
-b = float(input('Entre com o valor de b: '))
-c = float(input('Entre com o valor de c: '))
+import math
 
-delta = (b ** 2) - 4 * a * c
+a = float(input('Digite um valor para A: '))
+b = float(input('Digite um valor para B: '))
+c = float(input('Digite um valor para C: '))
 
-print("\n**************************\n")
+delta = (b*b)-(4*a*c)
 
-if a == 0:
-    print("O valor de a, deve ser diferente de 0")
-elif delta < 0:
-    print("Sem raízes reais")
+if delta < 0:
+	print('Esta equação não possui raízes reais.')
+elif delta == 0:
+	raiz1 = ((-1*b)+math.sqrt(delta))/(2*a)
+	print('A raiz desta equação é:', raiz1)
 else:
-    x1 = (-b + delta ** (1 / 2)) / (2 * a)
-    x2 = (-b - delta ** (1 / 2)) / (2 * a)
-
-    print("x1: {}, x2: {}".format(x1, x2))
+	raiz1 = ((-1*b)+math.sqrt(delta))/(2*a)
+	raiz2 = ((-1*b)-math.sqrt(delta))/(2*a)
+	if raiz1<raiz2:
+		print('As raízes da equação são', raiz1,'e',raiz2)
+	else:
+		print('As raízes da equação são', raiz2,'e',raiz1)
--- 3semana/pontos.py
+++ 3semana/pontos.py
@@ -1,25 +1,18 @@
 import math
- 
-def distancia(xa,ya, xb,yb):
-    """ Calcula a distancia entre dois pontos"""
-    dx = xb-xa
-    dy = yb-ya
-    d = math.sqrt(dx*dx + dy*dy)
-    return d    
- 
-x0 = float(input("Digite x0 da origem: "))
-y0 = float(input("Digite y0 da origem: "))
- 
-n = int(input("Digite n:"))
- 
-dmin = -1
-for i in range(0,n,1):
-    x = float(input("Digite x%d: "%(i+1)))
-    y = float(input("Digite y%d: "%(i+1)))
-    d = distancia(x0,y0, x,y)
-    if dmin < 0 or d < dmin:
-        dmin = d
-        xmin = x
-        ymin = y
- 
-print("x=%.2f, y=%.2f, d=%.2f"%(xmin,ymin,dmin))
+
+xA = float(input('Digite a abscissa do ponto A:'))
+xB = float(input('Digite a abscissa do ponto B:'))
+
+yA = float(input('Digite a ordenada do ponto A:'))
+yB = float(input('Digite a abscissa do ponto B:'))
+
+
+distAB = math.sqrt((xA-xB)**2 + ((yA-yB)**2))
+
+
+if (distAB<10.0):
+    print("Perto")
+else:
+    print("Longe")
+
+
